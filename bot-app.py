de820a66519b89354a94f10ee3de933be466121d
Airat <48244304+Faizullov@users.noreply.github.com>
2022-12-25 00:12:19 +0300

    Add files via upload

--- bot-app.py
+++ bot-app.py
@@ -40,6 +40,7 @@ def send_message(chat_id, text, name):
         access = 0
     else:                                                               # take access from first data in dataset
         access = local_coll.find_one({'saw': {"$gt": -1}})['command']   # take access for commands
+
     if text == "Только лучшее" and (access == 0):
         if str(chat_id) in db.list_collection_names():  # check if we've seen title
             local_coll = db[str(chat_id)]
@@ -55,19 +56,23 @@ def send_message(chat_id, text, name):
             local_coll.insert_one({'saw': 1, "lst_saw": [], "command": 0, "user_name": name})  # create new data
         local_coll.update_one({'saw': {"$gt": -1}}, {'$set': {'command': 1}})
         out = arr[db[str(chat_id)].find_one({'saw': {"$gt": -1}})['saw']]['link']
+
     elif text == "/start" and (access == 0):
         out = "Этот бот будет предлгать самые лучшие тайтлы, нажимай кнопку 'Только лучшее', чтобы увидеть их. " \
               "Также можно" \
               " отмечать понравившиеся аниме, чтобы было всегда к чему вернуться."
+
     elif text == "Bruh, man..." and (access == 1):
         out = "Посмотрим ещё"
         local_coll.update_one({'saw': {"$gt": -1}}, {'$set': {'command': 0}})
+
     elif text == "Not bad" and (access == 1):
         out = "Добавлено в список понравившегося"
         local_coll = db[str(chat_id)]
         tmp = local_coll.find_one({'saw': {"$gt": -1}})['saw']
         local_coll.insert_one({'name': arr[tmp]['title'], 'image': arr[tmp]['img_url']})  # add anime to dataset
         local_coll.update_one({'saw': {"$gt": -1}}, {'$set': {'command': 0}})
+
     elif text == "Список понравившегося" and (access == 0):
         local_coll = db[str(chat_id)]
         lst = []
@@ -83,12 +88,15 @@ def send_message(chat_id, text, name):
                 lst.append('\n')
             out = ''.join(lst)
         local_coll.update_one({'saw': {"$gt": -1}}, {'$set': {'command': 2}})
+
     elif text == "Что-то разонравилось или надоело" and (access == 2):
         out = "Введи номер из списка"
         local_coll.update_one({'saw': {"$gt": -1}}, {'$set': {'command': 3}})
+
     elif text == "Ой, случайно не добавил тайтл" and (access == 2):
         out = "Введи название тайтла"
         local_coll.update_one({'saw': {"$gt": -1}}, {'$set': {'command': 4}})
+
     elif text.isnumeric() and (access == 3):
         local_coll = db[str(chat_id)]
         if local_coll.find({'name': {"$gt": ""}}) is None:
@@ -107,9 +115,11 @@ def send_message(chat_id, text, name):
                         break
                 out = "Удалено"
         local_coll.update_one({'saw': {"$gt": -1}}, {'$set': {'command': 0}})
+
     elif text == "Посмотрел на свой список, всё норм" and (access == 2):
         out = "Перейдем к дальнейшему просмотру"
         local_coll.update_one({'saw': {"$gt": -1}}, {'$set': {'command': 0}})
+
     else:
         out = "Какая-то ошибка"
         if access != 4:                  # problems with access, mean we used wrong command
@@ -125,15 +135,18 @@ def send_message(chat_id, text, name):
                 local_coll.insert_one({'name': tmp['title'], 'image': tmp['img_url']})
                 out = "Добавлен новый тайтл"
             local_coll.update_one({'saw': {"$gt": -1}}, {'$set': {'command': 0}})
+
     method = "sendMessage"
     token = get_from_env("BOT_TOKEN")
     url = f"https://api.telegram.org/bot{token}/{method}"    # create url
+
     if text == "Только лучшее":                            # return data with text and buttons
         data = {"chat_id": chat_id, "text": out, "reply_markup": json.dumps({"keyboard": [[{"text": "Not bad"}],
                                                                                           [{
                                                                                               "text": "Bruh, man..."}]],
                                                                              "resize_keyboard": True,
                                                                              "one_time_keyboard": True})}
+
     elif text == "Список понравившегося":
         data = {"chat_id": chat_id, "text": out, "reply_markup": json.dumps({"keyboard": [[{"text": "Что-то "
                                                                                                     "разонравилось "
@@ -149,10 +162,13 @@ def send_message(chat_id, text, name):
                                                                                           ],
                                                                              "resize_keyboard": True,
                                                                              "one_time_keyboard": True})}
+
     elif text == "Что-то разонравилось или надоело":
         data = {"chat_id": chat_id, "text": out}
+
     elif text == "Ой, случайно не добавил тайтл":
         data = {"chat_id": chat_id, "text": out}
+
     else:
         data = {"chat_id": chat_id, "text": out, "reply_markup": json.dumps({"keyboard": [[{"text": "Только лучшее"}],
                                                                                           [{
